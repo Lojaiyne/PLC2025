4a441284faa8a9946120c2805373e464d45b30f3
Lojaiyne Hamdy <230049329@aston.ac.uk>
2025-01-27 16:12:51 +0000

    Task1,2,3,4 and func done

--- lab2-functionalprimer/haskell/task1.hs
+++ lab2-functionalprimer/haskell/task1.hs
@@ -3,9 +3,11 @@ module Main where
 main =
     do
     print (myFunc1 5)
-    --print (MyFunc2 10)
-    --print (3rdFunc)
+    print (myFunc2 10)
+    print (myFunc3)
+
+--functions start ONLY with lower letter or "_"
 
 myFunc1 x = x*10
---MyFunc2 x = x*2
---3rdFunc = "Hello, this is 3rd Func"
+myFunc2 x = x*2 --instead of MyFunc2
+myFunc3 = "Hello, this is 3rd Func" --instead of 3rdFun
--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -4,8 +4,11 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
-    else putStrLn ("you said: " ++ reverse line)
+    then ask (prompt ++ "!") --to add ! when entered without anything being typed
+    else if(line=="quit") then putStrLn ("quitting") --condition for quitting
+    else do 
+      putStrLn ("you said: " ++ reverse line)
+      ask prompt -- needed so programme continues after printing reverse line
 
 main :: IO ()
 main =
