a54fc492a578264a50bbd67e3c146bde164c6ca4
Lojaiyne Hamdy <230049329@aston.ac.uk>
2025-02-24 10:04:34 +0000

    Lab2

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,8 +1,19 @@
 --create inpFunc
-inpFunc a b = [a..b] --representing any integers
+--inpFunc a b = [a..b] --representing any integers
 
 --Define applicatorFunc
-applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else (sum (inpFunc a b))/ (b-a+1)  
+--applicatorFunc inpFunc a b s = if s=='s' then sum (inpFunc a b) else (sum (inpFunc a b))/ (b-a+1)  
+
+--main = do
+   -- let result = applicatorFunc inpFunc 1 10 's' --Call applicatorFunc with inpFunc, integers and char as args
+   -- putStrLn("sum = " ++ show(result))
+
+-- written as guards
+inpFunc a b = [a..b] --created inpFunc
+
+--define applicatorFunc
+applicatorFunc a b s | s=='s'       = sum (inpFunc a b)
+                     | otherwise (sum (inpFunc a b)) / (b-a+1)
 
 main = do
     let result = applicatorFunc inpFunc 1 10 's' --Call applicatorFunc with inpFunc, integers and char as args
